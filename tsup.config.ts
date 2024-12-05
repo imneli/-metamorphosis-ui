d06b980dfe6e70c2bbedb2d7d4c646d65c05d6bd
nekcyag <contatodoneli@gmail.com>
2024-12-05 11:46:32 -0300

    Tests : Package

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
   entry: ['src/ui/components/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
-  external: ['react', 'react-dom'],
+  external: [
+    'react',
+    'react-dom',
+    'class-variance-authority',
+    'clsx',
+    'tailwind-merge'
+  ],
   clean: true,
+  treeshake: true,
+  splitting: true,
+  sourcemap: true,
 });
