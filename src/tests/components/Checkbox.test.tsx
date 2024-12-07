295d61c7cee7de0fae159f0599a2a1f30aff507e
nekcyag <contatodoneli@gmail.com>
2024-12-06 22:50:40 -0300

    Fix - Error (Click)

--- src/tests/components/Checkbox.test.tsx
+++ src/tests/components/Checkbox.test.tsx
@@ -5,10 +5,12 @@ import { cn } from "@/lib/utils";
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ className, onChange, ...props }, ref) => {
+  ({ className, onChange, disabled, ...props }, ref) => {
     const [isChecked, setIsChecked] = React.useState(false);
 
     const handleClick = () => {
+      if (disabled) return;
+      
       setIsChecked(!isChecked);
       if (onChange) {
         const event = {
@@ -19,20 +21,30 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
     };
 
     return (
-      <div className="relative inline-flex" onClick={handleClick}>
+      <div 
+        className={cn(
+          "relative inline-flex",
+          disabled ? "cursor-not-allowed" : "cursor-pointer"
+        )} 
+        onClick={handleClick}
+      >
         <input
           type="checkbox"
           ref={ref}
           className="sr-only"
           checked={isChecked}
+          disabled={disabled}
           {...props}
         />
         <div
           className={cn(
             "h-5 w-5 rounded border transition-all duration-200",
-            "flex items-center justify-center cursor-pointer",
+            "flex items-center justify-center",
+            !disabled && "hover:border-gray-400",
+            !disabled && isChecked && "hover:bg-gray-900",
             isChecked ? "bg-black border-black text-white" : "border-gray-300 bg-white",
-            props.disabled && "opacity-50 cursor-not-allowed",
+            disabled && "opacity-50 bg-gray-100",
+            "focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-black",
             className
           )}
         >
